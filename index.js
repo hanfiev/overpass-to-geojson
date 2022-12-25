6c5716e98d7ddf08411ebde1b75edfa729a4cca5
Hanfie Vandanu <hanfiev@gmail.com>
2022-12-25 18:41:28 +0700

    add search radius polygon

--- index.js
+++ index.js
@@ -5,24 +5,13 @@ const port = 3000
 
 const {point} = require('@turf/helpers');
 const distance= require('@turf/distance').default;
-
+const buffer = require('@turf/buffer');
 
 app.use(cors())
-
-
 
 var lat,lon,radius,key,value
 var request = require('request');
 
-const calculateDistance = (data,lat,lon) => {
-  for(i=0; i<data.length; i++){
-    var from = point([lat,lon]);
-    var to = point([data.lat, data.lon]);
-    var distance = distance(from, to);
-    data[i].distance = distance
-  }
-  return(data)
-}
 //generate overpass api query
 app.get('/api/:lat/:lon/:radius/:key/:value', async (req, res) => {
   lat = req.params.lat
@@ -30,7 +19,6 @@ app.get('/api/:lat/:lon/:radius/:key/:value', async (req, res) => {
   radius = req.params.radius
   key = req.params.key
   value = req.params.value
-  // var query = '[out:json];(node["amenity"="restaurant"]('+lat+','+lon+','+radius+');way["amenity"="restaurant"]('+lat+','+lon+','+radius+');relation["amenity"="restaurant"]('+lat+','+lon+','+radius+'););out body;>;out skel qt;'
 
   var query = `[out:json];
   (
@@ -39,16 +27,15 @@ app.get('/api/:lat/:lon/:radius/:key/:value', async (req, res) => {
   out center;`
 
   apiURL = 'https://overpass-api.de/api/interpreter?data=' + query
-  
-  // request(apiURL).pipe(res)
+
 
   request(apiURL, async function (error, response, body) {
     let data = JSON.parse(body);
     data = data.elements
     
     // calculate distance
+    var from = point([lat,lon]);
     for(i=0; i<data.length; i++){
-      var from = point([lat,lon]);
       var to = point([data[i].lat, data[i].lon]);
       data[i].distance = distance(from, to);
     }
@@ -74,33 +61,20 @@ app.get('/api/:lat/:lon/:radius/:key/:value', async (req, res) => {
       delete data[i].timestamp
       delete data[i].distance
     }
+    // generate buffer area and push it to data
+    var centerPoint = point([parseFloat(lon),parseFloat(lat)]);
+    var area = buffer(centerPoint, radius*0.001, {units: 'kilometers'});
+    data.push(area)
 
     let geojson = {"type": "FeatureCollection", "features": data}
     let encodedURI = encodeURIComponent(JSON.stringify(geojson))
     let redirectURI = `http://geojson.io/#data=data:application/json,${encodedURI}`
     res.redirect(redirectURI);
 
-    // res.send(uri)
-    // res.send(calculateDistance(lat))
   });
 
-
-  // res.send(query)
 })
-
-
-
 
 app.listen(port, () => {
   // console.log(`app listening on port ${port}`)
 })
-
-
-// import buffer from '@turf/buffer';
-// import {point} from '@turf/helpers';
-
-
-
-
-
-// console.log(buffered)
