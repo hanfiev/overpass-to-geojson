d4e741819c8e3a84c42aa4583311b29a309b1d86
Hanfie Vandanu <hanfiev@gmail.com>
2023-01-05 18:51:47 +0700

    add new endpoint

--- index.js
+++ index.js
@@ -67,6 +67,66 @@ app.get('/api/:lat/:lon/:radius/:key/:value', async (req, res) => {
     data.push(area)
 
     let geojson = {"type": "FeatureCollection", "features": data}
+    res.redirect(geojson);
+
+  });
+
+})
+
+app.get('/geojson/:lat/:lon/:radius/:key/:value', async (req, res) => {
+  lat = req.params.lat
+  lon = req.params.lon
+  radius = req.params.radius
+  key = req.params.key
+  value = req.params.value
+
+  var query = `[out:json];
+  (
+    node["${key}"~"${value}"](around:${radius},${lat}, ${lon});
+  );
+  out center;`
+
+  apiURL = 'https://overpass-api.de/api/interpreter?data=' + query
+
+
+  request(apiURL, async function (error, response, body) {
+    let data = JSON.parse(body);
+    data = data.elements
+    
+    // calculate distance
+    var from = point([lat,lon]);
+    for(i=0; i<data.length; i++){
+      var to = point([data[i].lat, data[i].lon]);
+      data[i].distance = distance(from, to);
+    }
+
+    // convert to geojson
+    features = []
+    for(i=0; i<data.length; i++){
+      data[i].type = "Feature"
+      data[i].properties = data[i].tags
+      data[i].properties.distance = data[i].distance
+      data[i].geometry = {
+        "type": "Point",
+        "coordinates": [data[i].lon, data[i].lat]
+      }
+      delete data[i].lat
+      delete data[i].lon
+      delete data[i].tags
+      delete data[i].id
+      delete data[i].version
+      delete data[i].changeset
+      delete data[i].user
+      delete data[i].uid
+      delete data[i].timestamp
+      delete data[i].distance
+    }
+    // generate buffer area and push it to data
+    var centerPoint = point([parseFloat(lon),parseFloat(lat)]);
+    var area = buffer(centerPoint, radius*0.001, {units: 'kilometers'});
+    data.push(area)
+
+    let geojson = {"type": "FeatureCollection", "features": data}
     let encodedURI = encodeURIComponent(JSON.stringify(geojson))
     let redirectURI = `http://geojson.io/#data=data:application/json,${encodedURI}`
     res.redirect(redirectURI);
